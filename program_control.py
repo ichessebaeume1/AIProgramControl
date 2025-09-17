ad2588e9c34faf216cd9fd9f2dfbf8be8ea5443b
jonas.wriedt <wriedtjonas@gmail.com>
2025-09-17 19:45:11 +0200

    Remote Commit

--- program_control.py
+++ program_control.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 from pydantic import BaseModel, Field
 import json
 
-task = input("")
+task = input("Command")
 
 client = OpenAI(
     api_key="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",
