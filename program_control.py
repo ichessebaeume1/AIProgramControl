758bc71310890ee4fe0ed149a8f8d50b517f8f5e
jonas.wriedt <wriedtjonas@gmail.com>
2025-09-17 20:16:15 +0200

    Application Opener

--- program_control.py
+++ program_control.py
@@ -1,15 +1,16 @@
 from openai import OpenAI
 from pydantic import BaseModel, Field
 import json
+import subprocess
 
-task = input("Command")
+task = input("Command: ")
 
 client = OpenAI(
     api_key="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",
 )
 
 class ApplicationName(BaseModel):
-    a: list[str] = Field(..., description='The names of the applications or files mentioned by the user which he want to open.')
+    a: list[str] = Field(..., description='The names of the applications mentioned by the user which he wants to open.')
 
 messages = [
     {
@@ -22,7 +23,18 @@ response = client.chat.completions.parse(
     messages=messages,
     model="gpt-5",
     response_format=ApplicationName,
+    timeout=10
 )
 
-app = json.loads(response.choices[0].message.content)["a"].lower()
-print(app)
+apps = json.loads(response.choices[0].message.content)['a']   # {'a': ['explorer']}
+
+for app in apps:
+    print(f"Trying to open {app.lower()}...")
+    command = f"start {app.lower()}"
+
+    try:
+        subprocess.run(command, shell=True)
+    except FileNotFoundError:
+        print(f"Windows could not find {app}.")
+    except Exception as e:
+        print(f"Unknown Error: {e}")
